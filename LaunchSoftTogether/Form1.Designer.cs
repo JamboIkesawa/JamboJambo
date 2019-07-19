3ea0385311bfdeee5aa53988e1741426ffc881a0
JamboIkesawa <grassencer@gmail.com>
2019-07-20 00:19:22 +0900

    LaunchSoftTogetherを追加

---
+++ LaunchSoftTogether/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Launch_Soft_Together
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.dataGridView3 = new System.Windows.Forms.DataGridView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(31, 398);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(31, 31);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(481, 86);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(31, 149);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(481, 85);
+			this.dataGridView2.TabIndex = 1;
+			// 
+			// dataGridView3
+			// 
+			this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView3.Location = new System.Drawing.Point(31, 274);
+			this.dataGridView3.Name = "dataGridView3";
+			this.dataGridView3.Size = new System.Drawing.Size(481, 85);
+			this.dataGridView3.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(565, 58);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(229, 19);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(128, 398);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(880, 450);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.dataGridView3);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.DataGridView dataGridView3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ LaunchSoftTogether/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Launch_Soft_Together
+{
+	
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			LoadData();
+			LoadData2();
+			LoadData3();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Document2 doc = (Document2)dataGridView3.DataSource;
+
+			//foreach(Document2 d2 in doc)
+			//{
+
+			//}
+		}
+
+		public void LoadData()
+		{
+			List<Car> cars = new List<Car>()
+			{
+				new Car() { Maker = "Subaru", Model = "Impreza", Year = 2005, Price = 1800},
+				new Car() { Maker = "Ford", Model = "Mustang", Year = 1984, Price = 2100}
+			};
+
+			dataGridView1.DataSource = cars;
+		}
+
+		public void LoadData2()
+		{
+			List<Document> documents = new List<Document>()
+			{
+				new Document() { CheckBox = false, FileName = "Document1.txt", AddButton = true, DeleteButton = true },
+				new Document() { CheckBox = false, FileName = "Document2.html", AddButton = true, DeleteButton = true }
+			};
+
+			dataGridView2.DataSource = documents;
+		}
+
+		public void LoadData3()
+		{
+			List<Document2> document2s = new List<Document2>()
+			{
+				new Document2() { 起動する = true, ドキュメント名 = "ドキュメント１.xlsx", 追加 = false, 削除 = false },
+				new Document2() { 起動する = true, ドキュメント名 = "ソフトウェア１.exe", 追加 = false, 削除 = false }
+			};
+
+			dataGridView3.DataSource = document2s;
+		}
+
+	}
+
+	class Car
+	{
+		public string Maker { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public int Price { get; set; }
+	}
+
+	class Document
+	{
+		public bool CheckBox { get; set; }
+		public string FileName { get; set; }
+		public bool AddButton { get; set; }
+		public bool DeleteButton { get; set; }
+	}
+
+	class Document2
+	{
+		public bool 起動する { get; set; }
+		public string ドキュメント名 { get; set; }
+		public bool 追加 { get; set; }
+		public bool 削除 { get; set; }
+	}
+}
