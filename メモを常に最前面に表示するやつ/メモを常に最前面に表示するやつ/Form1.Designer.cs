e9dc072558bdb3e91eee9f8f4ba80fe75690ec3f
JamboIkesawa <grassesencer@gmail.com>
2019-10-11 17:38:24 +0900

    メモを常に最前面に表示するやつを追加

---
+++ メモを常に最前面に表示するやつ/メモを常に最前面に表示するやつ/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace メモを常に最前面に表示するやつ
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label_Title = new System.Windows.Forms.Label();
+			this.richTextBox_Memo = new System.Windows.Forms.RichTextBox();
+			this.button_Save = new System.Windows.Forms.Button();
+			this.button_Open = new System.Windows.Forms.Button();
+			this.checkBox_ReadOnly = new System.Windows.Forms.CheckBox();
+			this.checkBox_topMost = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label_Title
+			// 
+			this.label_Title.AutoSize = true;
+			this.label_Title.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label_Title.Location = new System.Drawing.Point(9, 10);
+			this.label_Title.Name = "label_Title";
+			this.label_Title.Size = new System.Drawing.Size(39, 13);
+			this.label_Title.TabIndex = 0;
+			this.label_Title.Text = "label1";
+			// 
+			// richTextBox_Memo
+			// 
+			this.richTextBox_Memo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox_Memo.Location = new System.Drawing.Point(13, 27);
+			this.richTextBox_Memo.Name = "richTextBox_Memo";
+			this.richTextBox_Memo.Size = new System.Drawing.Size(150, 107);
+			this.richTextBox_Memo.TabIndex = 1;
+			this.richTextBox_Memo.Text = "";
+			this.richTextBox_Memo.TextChanged += new System.EventHandler(this.richTextBox_Memo_TextChanged);
+			// 
+			// button_Save
+			// 
+			this.button_Save.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.button_Save.Location = new System.Drawing.Point(93, 159);
+			this.button_Save.Name = "button_Save";
+			this.button_Save.Size = new System.Drawing.Size(75, 23);
+			this.button_Save.TabIndex = 2;
+			this.button_Save.Text = "保存";
+			this.button_Save.UseVisualStyleBackColor = true;
+			this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+			// 
+			// button_Open
+			// 
+			this.button_Open.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.button_Open.Location = new System.Drawing.Point(12, 159);
+			this.button_Open.Name = "button_Open";
+			this.button_Open.Size = new System.Drawing.Size(75, 23);
+			this.button_Open.TabIndex = 2;
+			this.button_Open.Text = "開く";
+			this.button_Open.UseVisualStyleBackColor = true;
+			this.button_Open.Click += new System.EventHandler(this.button_Open_Click);
+			// 
+			// checkBox_ReadOnly
+			// 
+			this.checkBox_ReadOnly.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkBox_ReadOnly.AutoSize = true;
+			this.checkBox_ReadOnly.Location = new System.Drawing.Point(12, 140);
+			this.checkBox_ReadOnly.Name = "checkBox_ReadOnly";
+			this.checkBox_ReadOnly.Size = new System.Drawing.Size(91, 16);
+			this.checkBox_ReadOnly.TabIndex = 3;
+			this.checkBox_ReadOnly.Text = "読み込みのみ";
+			this.checkBox_ReadOnly.UseVisualStyleBackColor = true;
+			this.checkBox_ReadOnly.CheckedChanged += new System.EventHandler(this.checkBox_ReadOnly_CheckedChanged);
+			// 
+			// checkBox_topMost
+			// 
+			this.checkBox_topMost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkBox_topMost.AutoSize = true;
+			this.checkBox_topMost.Location = new System.Drawing.Point(104, 140);
+			this.checkBox_topMost.Name = "checkBox_topMost";
+			this.checkBox_topMost.Size = new System.Drawing.Size(72, 16);
+			this.checkBox_topMost.TabIndex = 3;
+			this.checkBox_topMost.Text = "最前表示";
+			this.checkBox_topMost.UseVisualStyleBackColor = true;
+			this.checkBox_topMost.CheckedChanged += new System.EventHandler(this.checkBox_TopMost_CheckedChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(175, 187);
+			this.Controls.Add(this.checkBox_topMost);
+			this.Controls.Add(this.checkBox_ReadOnly);
+			this.Controls.Add(this.button_Open);
+			this.Controls.Add(this.button_Save);
+			this.Controls.Add(this.richTextBox_Memo);
+			this.Controls.Add(this.label_Title);
+			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(191, 226);
+			this.Name = "Form1";
+			this.Text = "Form";
+			this.TopMost = true;
+			this.SizeChanged += new System.EventHandler(this.richTextBox_Memo_Resize);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label_Title;
+		private System.Windows.Forms.RichTextBox richTextBox_Memo;
+		private System.Windows.Forms.Button button_Save;
+		private System.Windows.Forms.Button button_Open;
+		private System.Windows.Forms.CheckBox checkBox_ReadOnly;
+		private System.Windows.Forms.CheckBox checkBox_topMost;
+	}
+}
+
---
+++ メモを常に最前面に表示するやつ/メモを常に最前面に表示するやつ/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace メモを常に最前面に表示するやつ
+{
+	public partial class Form1 : Form
+	{
+		const int checkboxSpan = 3;
+		const int buttonSpan = 11;
+		const string DocInitialName = "新規";
+		string memoDocName = "新規テキストドキュメント";
+
+		public Form1()
+		{
+			InitializeComponent();
+			label_Title.Text = DocInitialName;
+		}
+
+		private void button_Open_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = null;
+			Stream stream = null;
+
+			ofd = FileOpen();
+			if(ofd.ShowDialog() == DialogResult.OK)
+			{
+				label_Title.Text = Path.GetFileName(ofd.FileName.ToString());
+				stream = ofd.OpenFile();
+				if(stream != null)
+				{
+					StreamReader sr = new StreamReader(stream);
+					richTextBox_Memo.Text = sr.ReadToEnd();
+					sr.Close();
+					stream.Close();
+				}	// TODO:文字コードを勝手に決めてくれるようにしたい。
+			}
+			else
+			{
+				ofd.Reset();
+				ofd.Dispose();
+			}
+		}
+
+		private void button_Save_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog sfd = null;
+			Stream stream = null;
+
+			sfd = FileSave();
+			if(sfd.ShowDialog() == DialogResult.OK)
+			{
+				stream = sfd.FileName;
+				StreamWriter sw = new StreamWriter(stream);
+			}
+			else
+			{
+				sfd.Reset();
+				sfd.Dispose();
+			}
+		}
+
+		private void checkBox_ReadOnly_CheckedChanged(object sender, EventArgs e)
+		{
+			// チェックされたらテキストボックスを読み込みのみにする。
+			if(checkBox_ReadOnly.Checked == true)
+			{
+				richTextBox_Memo.ReadOnly = true;
+			}
+			else
+			{
+				richTextBox_Memo.ReadOnly = false;
+			}
+		}
+
+		private OpenFileDialog FileOpen()
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+
+			ofd.FileName = "";
+			ofd.InitialDirectory = Directory.GetCurrentDirectory();
+			ofd.Filter = "テキストファイル(*.txt)|*.txt|ログファイル(*.log)|*.log";
+			ofd.FilterIndex = 1;
+			ofd.Title = "開くファイルを選択してください";
+			ofd.RestoreDirectory = true;
+			ofd.Multiselect = false;    // TODO:後々タブみたいなの追加して複数選択できるようにしたい。
+
+			return ofd;
+		}
+
+		private SaveFileDialog FileSave()
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+
+			sfd.FileName = "";
+			sfd.InitialDirectory = "C:\\Desktop";
+			sfd.Filter = "テキストファイル(*.txt)|*.txt|ログファイル(*.log)|*.log";
+			sfd.FilterIndex = 1;
+			sfd.Title = "テキストをセーブします。";
+			sfd.RestoreDirectory = true;
+
+			return sfd;
+		}
+
+		private void richTextBox_Memo_Resize(object sender, EventArgs e)
+		{
+			int topToRichTextBox = 0;
+			int richTextBoxToCheck = 0;
+			int richTextBoxToButton = 0;
+
+			// TODO:+方向にしか座標が変化していないからボタンが戻ってこない。
+			topToRichTextBox = richTextBox_Memo.Location.Y + richTextBox_Memo.Size.Height;
+			richTextBoxToCheck = topToRichTextBox + 1;
+			richTextBoxToButton = richTextBoxToCheck + 1;
+
+			checkBox_ReadOnly.Location = new Point(checkBox_ReadOnly.Location.X, (checkBox_ReadOnly.Location.Y + richTextBoxToCheck) / 2 + checkboxSpan);
+			button_Open.Location = new Point(button_Open.Location.X, (button_Open.Location.Y + richTextBoxToButton) / 2 + buttonSpan);
+			button_Save.Location = new Point(button_Save.Location.X, (button_Save.Location.Y + richTextBoxToButton) / 2 + buttonSpan);
+		}
+
+		private void richTextBox_Memo_TextChanged(object sender, EventArgs e)
+		{
+			label_Title.Text = memoDocName;
+		}
+
+		private void checkBox_TopMost_CheckedChanged(object sender, EventArgs e)
+		{
+			if(checkBox_topMost.Checked == true)
+			{
+				this.TopMost = true;
+			}
+			else
+			{
+				this.TopMost = false;
+			}
+		}
+	}
+}
