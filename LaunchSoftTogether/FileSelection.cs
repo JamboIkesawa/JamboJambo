17702ae473c83432f13bd88ce16cf24034e096ed
JamboIkesawa <grassesencer@gmail.com>
2019-12-10 19:54:08 +0900

    FileSelectionのDataGridViewの表示を変更

--- LaunchSoftTogether/FileSelection.cs
+++ LaunchSoftTogether/FileSelection.cs
@@ -25,6 +25,7 @@ namespace Launch_Soft_Together
 			xmlFile = cc.OpenXmlFile();
 			dataGridView_FileList.DataSource = xmlFile;
 			checkBox_LaunchConfirm.Checked = config.PrevData;
+			ChangeGridViewStyle(dataGridView_FileList);
 		}
 
 		private void button_ListOpen_Click(object sender, EventArgs e)
@@ -48,6 +49,30 @@ namespace Launch_Soft_Together
 		private void FileSelection_FormClosing(object sender, FormClosingEventArgs e)
 		{
 			config.PrevData = checkBox_LaunchConfirm.Checked;
+		}
+
+		private void ChangeGridViewStyle(DataGridView changeGrid)
+		{
+			// 列ヘッダーを非表示にする
+			changeGrid.RowHeadersVisible = false;
+
+			// ユーザーが行列のサイズを調整できないようにする。
+			changeGrid.AllowUserToResizeRows = false;
+			changeGrid.AllowUserToResizeColumns = false;
+
+			// ファイルパスを非表示にする。
+			changeGrid.Columns[1].Visible = false;
+
+			// DataGridViewの列幅を調整する。
+			changeGrid.Columns[0].Width = changeGrid.Size.Width;
+
+			// 列の名前を変更する。
+			changeGrid.Columns[0].HeaderText = "XMLファイル名";
+
+			// ヘッダーの高さを調整する。
+			//changeGrid.Rows[0].Height = 100;
+			changeGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
 		}
 	}
 }
